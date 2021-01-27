4f3d53ca4a593ed28e7691f031ae4e719e882c78
jp <jp@jpls-Inspiron-5480>
2021-01-27 20:22:46 -0300

    criacao do projeto

---
+++ Loja_livros_on_line/src/test/java/br/edu/ifpb/dac/lojalivros/LojaLivrosOnLineApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.dac.lojalivros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaLivrosOnLineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
